dfb2f9b76fb64594355d5a95d3e4f27456466abb
Adithya Peruvemba <adithya.peruvemba@gmail.com>
2019-11-10 19:31:12 -0800

    Complete Checkpoint 2

--- FracCalc.java
+++ FracCalc.java
@@ -24,13 +24,19 @@ public class FracCalc {
         String firstoperandwhole = " ";
         String firstoperandnum = " ";
         String firstoperanddenom = " ";
+        String operator= input.substring(input.indexOf (" ")+1, input.indexOf (" ") + 2);
+        String secondoperand = input.substring(input.indexOf (" ") + 3);
+        String secondoperandwhole = " ";
+        String secondoperandnum = " ";
+        String secondoperanddenom = " ";
+
         if (firstoperand.contains("_") && firstoperand.contains("-")==false)
         {
             firstoperandwhole = firstoperand.substring(0, firstoperand.indexOf("_"));
             firstoperandnum = firstoperand.substring(firstoperand.indexOf("_"), firstoperand.indexOf("/"));
             firstoperanddenom = firstoperand.substring(firstoperand.indexOf("/")+1);
         }
-        else if(firstoperand.contains("_") && firstoperand.contains("-")== true)
+        else if(firstoperand.contains("_") && firstoperand.contains("-"))
         {
             firstoperandwhole = firstoperand.substring(firstoperand.indexOf("-")+1, firstoperand.indexOf("_"));
             firstoperandnum = firstoperand.substring(firstoperand.indexOf("_"), firstoperand.indexOf("/"));
@@ -44,26 +50,46 @@ public class FracCalc {
         else if (firstoperand.contains("_") == false && firstoperand.contains("/") && firstoperand.contains("-")){
             firstoperandwhole = "0" ;
             firstoperandnum = firstoperand.substring(firstoperand.indexOf("-")+1,firstoperand.indexOf("/"));
-            firstoperanddenom = firstoperand.substring(firstoperand.indexOf("/")+1, firstoperand.indexOf(" "));
+            firstoperanddenom = firstoperand.substring(firstoperand.indexOf("/")+1);
         }
-        else
-        {
+        else if (firstoperand.contains("_") == false && firstoperand.contains("/") == false && firstoperand.contains("-")){
+            firstoperandwhole = firstoperand.substring(firstoperand.indexOf("-")+1);
+            firstoperandnum = "0";
+            firstoperanddenom = "1";
+        }
+        else if (firstoperand.contains("_") == false && firstoperand.contains("/") == false && firstoperand.contains("-")== false){
             firstoperandwhole = firstoperand;
             firstoperandnum = "0";
             firstoperanddenom = "1";
-            /*String operator= input.substring(input.indexOf (" "), input.indexOf (" ") + 2);
-            String secondoperand = input.substring(input.indexOf (" ") + 2);
-            String secondoperandwhole = secondoperand.substring(0, secondoperand.indexOf("_"));
-            String secondoperandnum = secondoperand.substring(secondoperand.indexOf("_"), secondoperand.indexOf("/"));
-            String secondoperanddenom = secondoperand.substring(secondoperand.indexOf("/")+1);
-             */
-            //String secondcheckpoint = "Whole: " + secondoperandwhole + " " + "Numerator: " + secondoperandnum + " " + "Denominator: " + secondoperanddenom;
-
-            // TODO: Implement this function to produce the solution to the input
-            //return secondcheckpoint; 
         }
-        return firstoperanddenom;
+        if (secondoperand.contains("_") == false && secondoperand.contains("/") == false){
+            secondoperandwhole = secondoperand;
+            secondoperandnum = "0";
+            secondoperanddenom = "1";
+        }
+        else if (secondoperand.contains("_") && secondoperand.contains("/")){
+            secondoperandwhole = secondoperand.substring(0,secondoperand.indexOf("_"));
+            secondoperandnum = secondoperand.substring(secondoperand.indexOf("_")+1, secondoperand.indexOf("/"));
+            secondoperanddenom = secondoperand.substring(secondoperand.indexOf("/")+1);
+        }
+        else{
+            secondoperandwhole = "0";
+            secondoperandnum = secondoperand.substring(0, secondoperand.indexOf("/"));
+            secondoperanddenom = secondoperand.substring(secondoperand.indexOf("/")+1);
+        }
+        String secondcheckpoint = "Whole: " + secondoperandwhole + " " + "Numerator: " + secondoperandnum + " " + "Denominator: " + secondoperanddenom;
+        /*String operator= input.substring(input.indexOf (" "), input.indexOf (" ") + 2);
+        String secondoperand = input.substring(input.indexOf (" ") + 2);
+        String secondoperandwhole = secondoperand.substring(0, secondoperand.indexOf("_"));
+        String secondoperandnum = secondoperand.substring(secondoperand.indexOf("_"), secondoperand.indexOf("/"));
+        String secondoperanddenom = secondoperand.substring(secondoperand.indexOf("/")+1);
+         */
+        //String secondcheckpoint = "Whole: " + secondoperandwhole + " " + "Numerator: " + secondoperandnum + " " + "Denominator: " + secondoperanddenom;
 
+        // TODO: Implement this function to produce the solution to the input
+        //return secondcheckpoint; 
+        return secondcheckpoint;
     }
+
 }
 
